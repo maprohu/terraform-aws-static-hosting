c1fe95c9d642641fb16082b6acc05fae805ce0f8
Marton Papp <maprohu@gmail.com>
2023-11-27 16:22:33 +0000

    add readmes

---
+++ modules/github-oidc/outputs.tf
@@ -0,0 +1,3 @@
+output "role_arn" {
+    value = module.iam_github_oidc_role.arn
+}
--- modules/secret-arn/main.tf
+++ modules/secret-arn/main.tf
@@ -5,7 +5,3 @@ provider "aws" {
 data "aws_secretsmanager_secret" "by_name" {
   name = var.aws_secret_name
 }
-
-output "arn" {
-  value = data.aws_secretsmanager_secret.by_name.arn
-}
